52f19cc82ee97fb0031702c646a48b41e127193e
refact0r <34758569+refact0r@users.noreply.github.com>
2022-06-26 18:10:56 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'Yifan',
-		location: 'bellevue'
+		name: 'refact0r',
+		location: 'Seattle'
 	}
 });
 
